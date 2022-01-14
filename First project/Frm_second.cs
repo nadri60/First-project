a118b942121d0866f33df5ffe221630787cf7cfb
nadri60 <nadri.hadi@gmail.com>
2022-01-14 20:42:50 +0330

    add form Frm_Second

--- First project/Frm_second.cs
+++ First project/Frm_second.cs
@@ -10,16 +10,11 @@ using System.Windows.Forms;
 
 namespace First_project
 {
-    public partial class Form1 : Form
+    public partial class Frm_second : Form
     {
-        public Form1()
+        public Frm_second()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Test");
         }
     }
 }
